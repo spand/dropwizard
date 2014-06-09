53faabe87a0a6c7ad0263c19af15a4ad48572fa6
Johannes Ulfkjær Jensen <spand@cs.au.dk>
2014-06-09 13:43:54 +0200

    Working by adding @JsonSubTypes.

--- dropwizard-example/src/main/java/com/example/helloworld/ComplexType.java
+++ dropwizard-example/src/main/java/com/example/helloworld/ComplexType.java
@@ -1,9 +1,13 @@
 package com.example.helloworld;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.dropwizard.jackson.Discoverable;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(SpecificType.class)
+})
 public interface ComplexType extends Discoverable {
 
 }
